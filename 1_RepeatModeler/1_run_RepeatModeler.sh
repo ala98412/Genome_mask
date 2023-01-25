65f0db25538e79a7620b6ee2d8752cd65af4ba14
ala98412 <39360615+ala98412@users.noreply.github.com>
2023-01-25 23:17:55 +0800

    Update 1_run_RepeatModeler.sh

--- 1_RepeatModeler/1_run_RepeatModeler.sh
+++ 1_RepeatModeler/1_run_RepeatModeler.sh
@@ -2,13 +2,15 @@
 
 RepeatModeler=/home/why/Tools/RepeatMasker_Tools/RepeatModeler-2.0.2a/RepeatModeler
 BuildDatabase=/home/why/Tools/RepeatMasker_Tools/RepeatModeler-2.0.2a/BuildDatabase
-RepeatClassifier=/home/why/Tools/RepeatMasker_Tools/RepeatModeler-2.0.2a/RepeatClassifier
-repeatmasker=/home/why/Tools/RepeatMasker_Tools/RepeatMasker/RepeatMasker
 
+sp_name=Oe
 genome=/home/why/Juihung/Opsariichthys_bidens/raw_data/genome/Ob.genome.fasta
 
+threads=12
+
 echo "Start running BuildDataBase"
-$BuildDatabase -name Ob -engine rmblast $genome
+$BuildDatabase -name $sp_name -engine rmblast $genome
 
 echo "Start running RepeatModeler"
-$RepeatModeler -pa 30 -database Ob -engine rmblast
+$RepeatModeler -pa $threads -database $sp_name -engine rmblast
+
