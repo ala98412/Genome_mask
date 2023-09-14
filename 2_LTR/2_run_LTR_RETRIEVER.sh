a80568b4c5c87724626900c4b328c1c9673ccda6
ala98412 <39360615+ala98412@users.noreply.github.com>
2023-09-14 22:35:22 +0800

    Update 2_run_LTR_RETRIEVER.sh

--- 2_LTR/2_run_LTR_RETRIEVER.sh
+++ 2_LTR/2_run_LTR_RETRIEVER.sh
@@ -7,4 +7,4 @@ genome=/home/why/Juihung/Triplophysa_tibetana/rawdata/genome/Ttib.genome.newname
 scn=Ttib.genome.newname.fa.finder.combine.scn
 
 
-$LTR_RETRIEVER -genome $genome -inharvest $scn -threads 36 -repeatmasker $repeatmasker -cdhit_path $cdhit
+$LTR_RETRIEVER -genome $genome -inharvest $scn -threads 36 -noanno -repeatmasker $repeatmasker -cdhit_path $cdhit
