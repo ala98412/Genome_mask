aeb54dd57f328fb2138dbfbfb4bd7ccfceb1cd66
ala98412 <39360615+ala98412@users.noreply.github.com>
2023-09-14 15:52:53 +0800

    Delete 3_combine directory

--- 3_combine/combine.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cat ../1_RepeatModeler/Ob-families.fa.classified \
-	../2_LTR/Ob.genome.fasta.LTRlib.fa \
-	/home/why/Juihung/0_DataUsingForAnalysis/RepeatDataBase/*.fa \
-	/home/why/Juihung/0_DataUsingForAnalysis/RepeatDataBase/DatabaseFromRepeatMaskerLibrary/Cyprinidae.lib.fa > combined.fa
