e8ac4dfc4ac720d34a020bb1e4aea22f6ace6a00
ala98412 <39360615+ala98412@users.noreply.github.com>
2023-09-15 18:26:49 +0800

    Update 2_run_LTR_RETRIEVER.sh

--- 2_LTR/2_run_LTR_RETRIEVER.sh
+++ 2_LTR/2_run_LTR_RETRIEVER.sh
@@ -3,8 +3,8 @@ LTR_RETRIEVER=/home/why/Tools/RepeatMasker_Tools/LTR_retriever/LTR_retriever
 repeatmasker="/home/why/Tools/RepeatMasker_Tools/RepeatMasker/"
 cdhit="/home/why/Tools/RepeatMasker_Tools/cdhit"
 
-genome=/home/why/Juihung/Triplophysa_tibetana/rawdata/genome/Ttib.genome.newname.fa
-scn=Ttib.genome.newname.fa.finder.combine.scn
+genome=../../genome/Omac.genome.newname.fa
+scn=Omac.genome.newname.fa.finder.combine.scn
 
 
 $LTR_RETRIEVER -genome $genome -inharvest $scn -threads 36 -noanno -repeatmasker $repeatmasker -cdhit_path $cdhit
